dc2f82e721befa4f071c37f2ef4e1c97632eb376
ashanick <asha.sundararajan@gmail.com>
2021-06-19 16:40:45 +0530

    Savelakes new project

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,60 +3,59 @@ import './style.css'
 import Carousel from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 import { autoplayPlugin } from '@brainhubeu/react-carousel';
-import TeaserCard from '../../components/TeaserCard'
 import SideBar from '../../components/SideBar'
-import PCard from '../../components/UI/PCard'
 
-import mainImage from '../../blogPostImages/taralake3.jpg'
-import threatSlider from '../../blogPostImages/Threats.png'
-import lakeprofile from '../../blogPostImages/LakeProfile.png'
-
+import mainImage from '../../blogPostImages/NewLakeMain-1200-380.png'
+import threatSlider from '../../blogPostImages/THREATS.png'
+import lakeprofile from '../../blogPostImages/Outcome Actions-1200-380.png'
+import ican from '../../blogPostImages/Ican.png'
+import lakeData from '../../data/blog.json'
+import RenderTeaserCard from '../../components/UI/RenderTeaserCard'
 
 
 const Home = (props) => {
+    console.log(lakeData)
+
+    const tempData = lakeData.data
+
+    const renderBlogList = tempData.map((lake)=> {
+        return (
+            <div className="col-md-4" >
+            <RenderTeaserCard lake={lake} />
+            </div>
+        )
+    })
   
     return (
         <div>
             <section className="homePosts">
-                <div style={{width: `70%`, display: `flex`}}>
+                <div style={{width: `100%`, display: `flex`}}>
                     <Carousel
                         plugins={[
                         'infinite',
                         {
                         resolve: autoplayPlugin,
                         options: {
-                            interval: 2000,
+                            interval: 4000,
                         }
-                        },
+                        }
                     ]}   
                     animationSpeed={1000}
                     >
+                        <img src={mainImage} className="sliderimg" alt ="main3"/>
                         <img src={threatSlider} className="sliderimg" alt="main1" />
                         <img src={lakeprofile} className="sliderimg" alt="main2"/>
-                        <img src={mainImage} className="sliderimg" alt ="main3"/>
+                        <img src={ican} className="sliderimg" alt="main3"/>
                     </Carousel>
                     
-                </div>
-                <div className="homeSide" style={{padding: '10px 10px'}}>
-                    <PCard>
-                        <h2>Why should we save lakes?</h2>
-                        <h5>So .....</h5>
-                        <p>
-                            <ul>
-                                <li>The food you eat, even organic does not get chemically polluted water</li>
-                                <li>Your tax money is not frittered away but put to good use</li>
-                                <li>You leave a beautiful world for your grandchildren</li>
-                            </ul>
-                        </p>
-                    </PCard>
                 </div>
             </section>
 
             <section className="galleryPost">
-                <div style={{width: `70%`, display: `flex`}}>
-                        <TeaserCard/>
-                        {/* <TeaserCard />
-                        <TeaserCard /> */}
+                <div style={{display: `flex`}}>
+                <section style={{display: `flex`}}>
+            <div>{renderBlogList}</div>
+        </section>
                 </div>
                 <SideBar />
             </section>
